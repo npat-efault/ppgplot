07c719e518a76334fa34676864439c69a24796af
haavee <hverkouter@gmail.com>
2018-06-29 13:44:08 +0200

    pgqwin(), Python2+3 Linux/OSX compat, C.Bassa fork
    This version is a fork of:
        https://github.com/npat-efault/ppgplot
    
    merging in useful edits from:
        https://github.com/cbassa/ppgplot (C. Bassa fork)
    
    merged with edits to make ppgplot compileable and loadable on MacOSX and
    Linux Python2 and 3
    
    setup.py + src/__init__.py
        modified to support both Python2, Python3:
            print function, no '.has_key()' in Py3k
            import _ppgplot.* does not work under Py3k so rewritten to work in
            both 2 and 3
        X11 library paths:
            on (modern) Linux X11 is not in /usr/X11R6/... so try a few paths
            and add if they exist
        O/S support:
            C. Bassa setup.py did not compile out of the box on MacOSX so only
            useful edits migrated into this one
    
    src/_ppgplot.c
        modified to support both Python2, Python3:
            C. Bassa fork had most of the Py3k work done but was not backward
            compat with 2.7 so fixed that; now compiles fine under 2.7 and 3.6
    
        PGPLOT functions:
            - C. Bassa fork added pgerr[xy]
            - C. Bassa fork changed pgcurs/pgband behaviour
            - added pgqwin to query current window

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from distutils.sysconfig import get_python_inc, get_python_lib
 import os
 import sys
@@ -26,7 +27,7 @@ try:
     include_dirs.extend(get_numpy_include_dirs())
     define_macros.append(('USE_NUMPY', None))
     undef_macros.append('USE_NUMARRAY')
-    print >>sys.stderr, "using numpy..."
+    print("using numpy...", file=sys.stderr)
     found_module = True
     # uncommenting the following line retains any previous ppgplot
     # package and installs this numpy-compatible version as
@@ -37,43 +38,43 @@ except ImportError:
 
 if not found_module:
     try:
-	# Try to use the "numarray" module (2nd option)
-	# uncomment the following line to disable usage of numarray
-	#raise ImportError
-	from distutils.core import setup
-	from numarray.numarrayext import NumarrayExtension
-	make_extension = NumarrayExtension
-	define_macros.append(('USE_NUMARRAY', None))
-	print >>sys.stderr, "using numarray..."
-	found_module = True
-	# uncommenting the following line retains any previous ppgplot
-	# package and installs this numpy-compatible version as
-	# the package ppgplot_numpy
-	#name = "ppgplot_numarray"
+        # Try to use the "numarray" module (2nd option)
+        # uncomment the following line to disable usage of numarray
+        #raise ImportError
+        from distutils.core import setup
+        from numarray.numarrayext import NumarrayExtension
+        make_extension = NumarrayExtension
+        define_macros.append(('USE_NUMARRAY', None))
+        print("using numarray...", file=sys.stderr)
+        found_module = True
+        # uncommenting the following line retains any previous ppgplot
+        # package and installs this numpy-compatible version as
+        # the package ppgplot_numpy
+        #name = "ppgplot_numarray"
     except ImportError:
-	pass
+        pass
 
 if not found_module:
     try:
-	# Try to use the "Numeric" module (3rd option)
-	# uncomment the following line to disable usage of Numeric
-	#raise ImportError
-	from distutils.core import setup, Extension
-	make_extension = Extension
- 	include_dirs.append(
-	    os.path.join(get_python_inc(plat_specific=1), "Numeric"))
-	undef_macros.append('USE_NUMARRAY')
-	print >>sys.stderr, "using Numeric..."
-	found_module = True
-	# uncommenting the following line retains any previous ppgplot
-	# package and installs this numpy-compatible version as
-	# the package ppgplot_numpy
-	#name = "ppgplot_Numeric"
+        # Try to use the "Numeric" module (3rd option)
+        # uncomment the following line to disable usage of Numeric
+        #raise ImportError
+        from distutils.core import setup, Extension
+        make_extension = Extension
+        include_dirs.append(
+            os.path.join(get_python_inc(plat_specific=1), "Numeric"))
+        undef_macros.append('USE_NUMARRAY')
+        print("using Numeric...", file=sys.stderr)
+        found_module = True
+        # uncommenting the following line retains any previous ppgplot
+        # package and installs this numpy-compatible version as
+        # the package ppgplot_numpy
+        #name = "ppgplot_Numeric"
     except ImportError:
-	pass
+        pass
 
 if not found_module:
-    raise Exception, "None of numpy, numarray or Numeric found"
+    raise Exception("None of numpy, numarray or Numeric found")
 
 if os.name == "posix":
     #libraries.append("png")
@@ -82,27 +83,29 @@ if os.name == "posix":
     # comment out g2c if compiling with gfortran (typical nowadays)
     # you may still need this if using an earlier fortran compiler
     # libraries.append("g2c")
-    library_dirs.append("/usr/X11R6/lib/")
-    if os.environ.has_key("PGPLOT_DIR"):
+    for ld in filter(os.path.isdir, ["/usr/lib/x86_64-linux-gnu/", "/usr/X11R6/lib/"]):
+        library_dirs.append(ld)
+    #
+    if 'PGPLOT_DIR' in os.environ:
         library_dirs.append(os.environ["PGPLOT_DIR"])
         include_dirs.append(os.environ["PGPLOT_DIR"])
     # locate Aquaterm dynamic library if running Mac OS X SCISOFT
     # (www.stecf.org/macosxscisoft/)
-    elif os.environ.has_key("SCIDIR"):
-	libraries.append("aquaterm")
+    elif 'SCIDIR' in os.environ:
+        libraries.append("aquaterm")
         library_dirs.append(os.path.join(os.environ["SCIDIR"], 'lib'))
     else:
-        print >>sys.stderr, "PGPLOT_DIR env var not defined!"
+        print("PGPLOT_DIR env var not defined!", file=sys.stderr)
 else:
-    raise Exception, "os not supported"
+    raise Exception("os not supported")
 
 ext_ppgplot = make_extension(name+'._ppgplot',
-			[os.path.join('src', '_ppgplot.c')],
-			include_dirs=include_dirs,
-			libraries=libraries,
-			library_dirs=library_dirs,
-			define_macros=define_macros,
-			extra_compile_args=extra_compile_args)
+            [os.path.join('src', '_ppgplot.c')],
+            include_dirs=include_dirs,
+            libraries=libraries,
+            library_dirs=library_dirs,
+            define_macros=define_macros,
+            extra_compile_args=extra_compile_args)
 
 
 
